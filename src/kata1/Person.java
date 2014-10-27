ce34559879cd7692e917b5381394be3156618ff6
usuario <usuario@10.22.143.10>
2014-10-27 12:39:09 +0000

    Second implementation

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -6,10 +6,14 @@ public class Person {
 
     private final String name;
     private final Date birthday;
+    private final long MILLISECONDS_PER_YEAR;
+    private final Date now;
     
     public Person(String name, Date birthday) {
         this.name = name;
         this.birthday = birthday;
+        this.MILLISECONDS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
+        this.now = new Date();
     }
 
     public Date getBirthday() {
@@ -20,12 +24,15 @@ public class Person {
         return name;
     }
 
-    public int getAge() {
-        return (int) (((((((getYear() / 1000) / 60) / 60) / 24) / 365)));
+    public int getAge(){
+        return millisecondsToYears(getAgeInMilliseconds());
     }
-
-    private long getYear() {
-        Date now = new Date();
+    
+    private int millisecondsToYears(long milliseconds){
+        return (int) (milliseconds / MILLISECONDS_PER_YEAR);
+    }
+    
+    private long getAgeInMilliseconds(){
         return now.getTime() - birthday.getTime();
     }
     
